5c9b66996522e14cc1702b7e39d76043bd28efdc
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-19 13:05:17 -0700

    Empty product controller, ready to start working

--- StoreApiTest/Controllers/ProductController.cs
+++ StoreApiTest/Controllers/ProductController.cs
@@ -13,41 +13,6 @@ namespace StoreApiTest.Controllers
         public ProductController(IGenericRepository<Product> _repository) =>
             this._repository = _repository;
 
-        [HttpGet]
-        public async Task test()
-        {
-            var p = new Product { Description = "", Image = "url...", Price = 30 };
-            await _repository.Create(p);
-            await _repository.Save();
-        }
-
-        [HttpGet("otro/{id}")]
-        public async Task<Product?> get(int id)
-        {
-            return await this._repository.GetById(id);
-        }
-
-
-        [HttpDelete("{id}")]
-        public async Task Delete(int id)
-        {
-            await _repository.Delete(id);
-            await _repository.Save();
-        }
-
-        [HttpPut("{id}")]
-        public async Task Update(int id)
-        {
-            var p = await _repository.GetById(id);
-
-            if (p is not null)
-            {
-                p.Price = 9999;
-                _repository.Update(p);
-                await _repository.Save();
-            }
-
-        }
 
     }
 }
