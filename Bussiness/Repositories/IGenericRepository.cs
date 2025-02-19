bd391ad4e706ea4e942d47c58ce51bf60b8db428
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-18 18:20:56 -0700

    Generic repository added

---
+++ Bussiness/Repositories/IGenericRepository.cs
@@ -0,0 +1,80 @@
+﻿using Bussiness.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bussiness.Repositories
+{
+    public interface IGenericRepository<T>
+    {
+        Task<T> Create(T entity);
+
+        IQueryable<T> GetAll();
+        Task<T?> GetById(int id);
+        Task<bool> Exist(int id);
+
+        void Updated(T entity);
+        Task<bool> Delete(int id);
+
+
+        Task<int> GetCount();
+        Task<int> GetCountNoFiltters();
+
+
+        Task<int> Save();
+    }
+
+    public  abstract partial class GenericRepository<T> : IGenericRepository<T> 
+        where T : BaseModel
+    {
+        private readonly DbContext _context;
+        protected DbSet<T> Entities => _context.Set<T>();
+
+        protected GenericRepository(DbContext context) =>
+            _context = context;
+
+    }
+
+    //CRUD:
+    public abstract partial class GenericRepository<T>
+    {
+        public IQueryable<T> GetAll() => Entities;
+
+        public async Task<T> Create(T entity) =>
+            (await Entities.AddAsync(entity)).Entity;
+
+        public async Task<bool> Exist(int id) =>
+            await Entities.AnyAsync(e => e.Id == id);
+
+
+
+        public Task<T?> GetById(int id) =>
+            Entities.FirstOrDefaultAsync(e => e.Id == id);
+
+        public async Task<bool> Delete(int id)
+        {
+            T? res = await Entities.FirstOrDefaultAsync(e => e.Id == id);
+
+            if (res is null)
+                return false;
+
+            Entities.Remove(res);
+            return true;
+        }
+
+        public void Updated(T entity) =>
+            Entities.Update(entity);
+
+    }
+
+    //Tools:
+    public abstract partial class GenericRepository<T>
+    {
+        public Task<int> GetCount() =>
+            Entities.CountAsync();
+
+        public Task<int> GetCountNoFiltters() =>
+            Entities.IgnoreQueryFilters().CountAsync();
+
+        public async Task<int> Save() =>
+            await _context.SaveChangesAsync();
+    }
+}
