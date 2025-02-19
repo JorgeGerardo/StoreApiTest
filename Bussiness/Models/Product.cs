a5390740d0c4cd6ff1265a427e9f5de17d5e2cd7
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-19 13:41:13 -0700

    Title added on Product create DTO

--- Bussiness/Models/Product.cs
+++ Bussiness/Models/Product.cs
@@ -10,6 +10,7 @@
 
     public class ProductCreateDTO
     {
+        public required string Title { get; set; }
         public required string Description { get; set; }
         public required float Price { get; set; }
         public required string Image { get; set; }
