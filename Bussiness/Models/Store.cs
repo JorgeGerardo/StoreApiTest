70a73e3daa9aab1d42c5fb560db0b9d1a4277966
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-18 18:26:19 -0700

    Fixed field name

--- Bussiness/Models/Store.cs
+++ Bussiness/Models/Store.cs
@@ -2,7 +2,7 @@
 {
     public class Store : BaseModel
     {
-        public required string Surcursal { get; set; }
+        public required string Sucursal { get; set; }
         public required string Address { get; set; }
 
         public IEnumerable<ProductStore> Products { get; set; } = new List<ProductStore>();
