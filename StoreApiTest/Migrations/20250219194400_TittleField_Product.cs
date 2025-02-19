051c260262f5ad334b77426294017ed0e3363bc7
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-19 12:47:45 -0700

    Migration to add title field on Product model are completed

---
+++ StoreApiTest/Migrations/20250219194400_TittleField_Product.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreApiTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class TittleField_Product : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Products");
+        }
+    }
+}
