801f818b75af6693d58a582fe7da7be599b4b6cc
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-19 13:33:42 -0700

    Generic controller added

---
+++ StoreApiTest/Controllers/GenericController.cs
@@ -0,0 +1,106 @@
+﻿using Bussiness.Models;
+using Bussiness.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreApiTest.Controllers
+{
+    public abstract partial class GenericController<T, TRepository, TCreateDTO, TUpdateDTO> : ControllerBase
+        where T : BaseModel
+        where TRepository : IGenericRepository<T>
+    {
+        private readonly IGenericRepository<T> _repository;
+        protected GenericController(IGenericRepository<T> reposotory) =>
+            _repository = reposotory;
+
+    }
+
+    //Get's:
+    public abstract partial class GenericController<T, TRepository, TCreateDTO, TUpdateDTO>
+    {
+        [HttpGet]
+        public virtual async Task<IEnumerable<T>> Get(int page = 0, int? pageSize = null) =>
+            await setPagination(page, pageSize).ToListAsync();
+
+        [HttpGet("{id}"), Authorize]
+        public virtual async Task<ActionResult<T>> GetById(int id)
+        {
+            T? res = await _repository.GetById(id);
+            return res is not null ? res : NotFound();
+        }
+    }
+
+
+    //CxUD (CRUD):
+    public abstract partial class GenericController<T, TRepository, TCreateDTO, TUpdateDTO>
+    {
+        //TODO: Update endpoint is missing
+
+
+        [HttpPost]
+        public virtual async Task<IActionResult> Add(TCreateDTO createDto)
+        {
+            try
+            {
+                T newEntity = MapToEntity(createDto);
+                await _repository.Create(newEntity);
+                await _repository.Save();
+
+                if (newEntity is null)
+                    return StatusCode(500, new ProblemDetails { Detail = "Error creating entity." });
+
+                return CreatedAtAction(nameof(GetById), new { newEntity.Id }, newEntity);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new ProblemDetails { Detail = e.Message });
+            }
+        }
+
+        [HttpDelete]
+        public virtual async Task<IActionResult> HardDelete(int id)
+        {
+            bool isDeleted = await _repository.Delete(id);
+            await _repository.Save();
+
+            return isDeleted ? NoContent() : NotFound();
+        }
+
+    }
+
+
+    //Maps:
+    public abstract partial class GenericController<T, TRepository, TCreateDTO, TUpdateDTO>
+    {
+        protected abstract T MapToEntity(TCreateDTO dto);
+        protected virtual Task<T> MapToEntityAsync(TCreateDTO dto) =>
+            Task.FromResult(MapToEntity(dto));
+
+        //Update
+        protected abstract T MapToUpdateEntity(T currentState, TUpdateDTO dto);
+        protected virtual Task<T> MapToUpdateEntityAsync(T currentState, TUpdateDTO dto) =>
+            Task.FromResult(MapToUpdateEntity(currentState, dto));
+
+    }
+
+    //Tools:
+    public abstract partial class GenericController<T, TRepository, TCreateDTO, TUpdateDTO>
+    {
+        private const int DEFAULT_PAGE_SIZE = 10;
+        private IQueryable<T> setPagination(int page, int? pageSize = null, IQueryable<T>? query = null)
+        {
+            if (query is null)
+                query = _repository.GetAll();
+
+            query = query.Skip((pageSize ?? DEFAULT_PAGE_SIZE) * page);
+
+            if (pageSize is not null)
+                query = query.Take(pageSize.Value);
+
+            return query;
+        }
+    }
+
+}
