d3346c92313c73939ceafda412596be755323b8c
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-19 00:56:01 -0700

    Product repository added

---
+++ Bussiness/Repositories/ProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Bussiness.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bussiness.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>
+    {
+        public ProductRepository(DbContext context) : base(context) { }
+    }
+}
