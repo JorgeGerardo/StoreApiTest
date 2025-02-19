30347855dc05850cc1acabba442ae3ccfda4e9fb
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-19 12:43:10 -0700

    Title added on Product model

--- Bussiness/Models/Product.cs
+++ Bussiness/Models/Product.cs
@@ -2,6 +2,7 @@
 {
     public class Product : BaseModel
     {
+        public required string Title { get; set; }
         public required string Description { get; set; }
         public required float Price { get; set; }
         public required string Image { get; set; }
