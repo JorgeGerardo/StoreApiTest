5efb2dc1dbb0d58302feae3c7548a5a2e121004e
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-18 21:33:53 -0700

    DIService added (all scopeds are here)

---
+++ StoreApiTest/Services/DIService.cs
@@ -0,0 +1,17 @@
+﻿using Bussiness.Models;
+using Bussiness.Repositories;
+using Microsoft.EntityFrameworkCore;
+using StoreApiTest.Data;
+
+namespace StoreApiTest.Services
+{
+    public static class DIService
+    {
+
+        public static void InjectDependencies(WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<DbContext, StoreContext>();
+            builder.Services.AddScoped<IGenericRepository<Product>, ProductRepository>();
+        }
+    }
+}
