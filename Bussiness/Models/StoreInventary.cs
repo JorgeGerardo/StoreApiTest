75758e9223addfaba6d332847513892301211de0
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-20 15:01:39 -0700

    Fixed Create and Update DTO model

--- Bussiness/Models/StoreInventary.cs
+++ Bussiness/Models/StoreInventary.cs
@@ -1,4 +1,6 @@
-﻿namespace Bussiness.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Bussiness.Models
 {
     public class StoreInventary : BaseModel
     {
@@ -10,28 +12,21 @@
 
         public required int Stock { get; set; }
 
+        [JsonIgnore]
         public IEnumerable<InventaryTransaction> Transacctions { get; set; } = new List<InventaryTransaction>();
     }
 
     public class StoreInventaryCreateDTO
     {
         public int ProductId { get; set; }
-        public Product? Product { get; set; }
         public int StoreId { get; set; }
-        public Store? Store { get; set; }
         public required int Stock { get; set; }
     }
 
     public class StoreInventaryUpdateDTO : BaseModel
     {
         public required int ProductId { get; set; }
-        public Product? Product { get; set; }
-
         public required int StoreId { get; set; }
-        public Store? Store { get; set; }
-
         public required int Stock { get; set; }
-
-
     }
 }
