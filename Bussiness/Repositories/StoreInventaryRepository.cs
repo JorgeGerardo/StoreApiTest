7c50aea62ae71184efe278a5518c0f3445e290d7
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-20 14:03:42 -0700

    Change to public class

--- Bussiness/Repositories/StoreInventaryRepository.cs
+++ Bussiness/Repositories/StoreInventaryRepository.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Bussiness.Repositories
 {
-    internal class StoreInventaryRepository : GenericRepository<StoreInventary>
+    public class StoreInventaryRepository : GenericRepository<StoreInventary>
     {
         public StoreInventaryRepository(DbContext context) : base(context) { }
     }
