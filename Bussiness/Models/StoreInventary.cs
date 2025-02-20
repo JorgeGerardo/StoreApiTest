26a3c52039f9297be5ffa7592be81057e68a3332
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-20 13:23:22 -0700

    Remove task

--- Bussiness/Models/StoreInventary.cs
+++ Bussiness/Models/StoreInventary.cs
@@ -13,20 +13,6 @@
         public IEnumerable<InventaryTransaction> Transacctions { get; set; } = new List<InventaryTransaction>();
     }
 
-    //TODO: Tenemos un problema de diseño, lo que sucede es lo siguiente
-    /*
-     * Primero: Stock no puede ir en en la tabla products por que solamente estaría contabilizando 
-     * el stock total y no el de cada tienda.
-     * 
-     * Segundo: Esta tabla no tiene sentido su campo Date porqué no se tendría un registro de cuales 
-     * veces y fechas se han hecho entradas y salidas de articulos.
-     *  
-     *  Posible soución: Hacer una tabla extra para manipular las entradas y qué esta tabla sea 
-     *  únicamente del stock total de un producto en una tienda.
-     *  
-     *  La tabla esa tendrá la fecha y el StoreId como ProductIt y deberá estar relacionada a una 
-     *  tabla intermedia
-     */
     public class StoreInventaryCreateDTO
     {
         public int ProductId { get; set; }
