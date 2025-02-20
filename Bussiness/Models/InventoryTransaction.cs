8ff1fb0553fa9a052f40aafb333932f3a97bf400
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-20 12:02:16 -0700

    inventory transaction model added

---
+++ Bussiness/Models/InventoryTransaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness.Models
+{
+    public class InventoryTransaction : BaseModel
+    {
+        public required int Quatity { get; set; }
+        public required DateTime Date { get; set; }
+
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+
+        public int StoreId { get; set; }
+        public Store? Store { get; set; }
+
+    }
+
+    public class InventoryTransactionCreateDTO
+    {
+        public required int Quatity { get; set; }
+        public int ProductId { get; set; }
+        public int StoreId { get; set; }
+    }
+
+    public class InventoryTransactionUpdateDTO : InventoryTransactionCreateDTO { }
+
+}
