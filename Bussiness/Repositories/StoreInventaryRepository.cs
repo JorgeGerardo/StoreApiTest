fde8d8bf347db819ff1b73c1c0841f3171a4da9b
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-20 12:48:59 -0700

    Rename repository

---
+++ Bussiness/Repositories/StoreInventaryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Bussiness.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bussiness.Repositories
+{
+    internal class StoreInventaryRepository : GenericRepository<StoreInventary>
+    {
+        public StoreInventaryRepository(DbContext context) : base(context) { }
+    }
+}
