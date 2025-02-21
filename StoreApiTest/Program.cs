9ffb6d63463b15e0dac5a555568847bf30d24eeb
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-21 03:40:40 -0700

    Auth setted

--- StoreApiTest/Program.cs
+++ StoreApiTest/Program.cs
@@ -1,9 +1,14 @@
 
 using Bussiness.Models;
 using Bussiness.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using StoreApiTest.Data;
 using StoreApiTest.Services;
+using System.Security.Claims;
+using System.Text;
 
 namespace StoreApiTest
 {
@@ -33,10 +38,66 @@ namespace StoreApiTest
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Planify", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+
+
+
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+
+                        ValidIssuer = builder.Configuration["JWT:Issuer"],
+                        ValidAudience = builder.Configuration["JWT:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                                Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]!)
+                        ),
+
+                        ClockSkew = TimeSpan.Zero,
+                        NameClaimType = "UserId",
+                        RoleClaimType = ClaimTypes.Role,
+                    }
+                );
+
 
             var app = builder.Build();
             app.UseCors("Cors_Angular");
+            app.UseAuthentication();
+            app.UseAuthorization();
 
 
             // Configure the HTTP request pipeline.
