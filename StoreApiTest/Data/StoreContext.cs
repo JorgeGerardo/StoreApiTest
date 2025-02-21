d6fef4176fc4699c383595ced9b04194310b8313
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-21 01:18:59 -0700

    Data seed added

--- StoreApiTest/Data/StoreContext.cs
+++ StoreApiTest/Data/StoreContext.cs
@@ -115,7 +115,256 @@ namespace StoreApiTest.Data
                         Description = "Es un pantalón blanco de pijama para dama",
                         Price = 723,
                         Image = "https://i.imgur.com/mp3rUty.jpeg",
+                    },
+                    new Product 
+                    {
+                        Id = 10,
+                        Title = "Tenis para futbool",
+                        Description = "Son unos tenis brillantes muy bonitos",
+                        Price = 2300,
+                        Image = "https://i.imgur.com/qNOjJje.jpeg",
+                    },
+                    new Product 
+                    {
+                        Id = 11,
+                        Title = "Sofa negro",
+                        Description = "Es un sofa muy comodo para cuando te visite la familia",
+                        Price = 5600,
+                        Image = "https://i.imgur.com/Qphac99.jpeg",
+                    },
+                    new Product 
+                    {
+                        Id = 12,
+                        Title = "Iphone 17 Pro Max Plus",
+                        Description = "Es el nuevo Iphone con un chip A19 Bionic",
+                        Price = 28000,
+                        Image = "https://m.media-amazon.com/images/I/71ZzBv49bTL.jpg",
+                    },
+                    new Product 
+                    {
+                        Id = 13,
+                        Title = "Gorro rojo",
+                        Description = "Es un gorro con cordones",
+                        Price = 599,
+                        Image = "https://i.imgur.com/kg1ZhhH.jpeg",
+                    },
+                    new Product 
+                    {
+                        Id = 14,
+                        Title = "Vaso de cristal",
+                        Description = "Es un vaso de cristal con una naranja dentro",
+                        Price = 246,
+                        Image = "https://i.imgur.com/b7trwCv.jpeg",
+                    },
+                    new Product 
+                    {
+                        Id = 15,
+                        Title = "Polystation Controller",
+                        Description = "No es un control del polystation pero suena bien",
+                        Price = 333,
+                        Image = "https://i.imgur.com/ZANVnHE.jpeg",
+                    },
+                    new Product 
+                    {
+                        Id = 16,
+                        Title = "Jugo de fruta",
+                        Description = "Deliciosa bebida de fruta fresca",
+                        Price = 199,
+                        Image = "https://i.imgur.com/BLDByXP.jpeg",
+                    },
+                    new Product 
+                    {
+                        Id = 17,
+                        Title = "Sudadera roja",
+                        Description = "Sudadera tipo cholo de barrio, ni el Brayan se te acercará",
+                        Price = 399,
+                        Image = "https://iili.io/dAyUpkJ.jpg",
+                    },
+                    new Product 
+                    {
+                        Id = 18,
+                        Title = "Vasos de cristal",
+                        Description = "Vasos para tomar agua cuando tengas mucha sed",
+                        Price = 799,
+                        Image = "https://i.imgur.com/TF0pXdL.jpeg",
+                    },
+                    new Product 
+                    {
+                        Id = 19,
+                        Title = "Sudadera roja",
+                        Description = "Perfecto conjunto para cuando te disfraces de santa",
+                        Price = 333,
+                        Image = "https://i.imgur.com/FDwQgLy.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 20,
+                        Title = "Gorra de color Azul",
+                        Description = "Ideal para protegerte del sol con estilo.",
+                        Price = 150,
+                        Image = "https://i.imgur.com/N1GkCIR.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 21,
+                        Title = "Camiseta de color Verde",
+                        Description = "Perfecta para días casuales y relajados.",
+                        Price = 200,
+                        Image = "https://i.imgur.com/cBuLvBi.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 22,
+                        Title = "Sudadera de color Gris",
+                        Description = "Abrígate con comodidad y un toque moderno.",
+                        Price = 350,
+                        Image = "https://i.imgur.com/KcT6BE0.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 23,
+                        Title = "Pantalón de color Negro",
+                        Description = "Versátil y cómodo para cualquier ocasión.",
+                        Price = 400,
+                        Image = "https://i.imgur.com/BZrIEmb.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 24,
+                        Title = "Chamarra de color Rojo",
+                        Description = "Para mantenerte abrigado con un toque llamativo.",
+                        Price = 550,
+                        Image = "https://i.imgur.com/wXuQ7bm.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 25,
+                        Title = "Bufanda de color Morado",
+                        Description = "Accesorio ideal para el invierno.",
+                        Price = 120,
+                        Image = "https://i.imgur.com/76HAxcA.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 26,
+                        Title = "Sombrero de color Beige",
+                        Description = "Perfecto para un look veraniego.",
+                        Price = 180,
+                        Image = "https://i.imgur.com/Wv2KTsf.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 27,
+                        Title = "Zapatos de color Marrón",
+                        Description = "Comodidad y estilo en cada paso.",
+                        Price = 600,
+                        Image = "https://i.imgur.com/R3iobJA.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 28,
+                        Title = "Suéter de color Amarillo",
+                        Description = "Ideal para darle vida a tu outfit.",
+                        Price = 300,
+                        Image = "https://i.imgur.com/JQRGIc2.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 29,
+                        Title = "Chaleco de color Naranja",
+                        Description = "Añade una capa extra de calidez y estilo.",
+                        Price = 250,
+                        Image = "https://i.imgur.com/mp3rUty.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 30,
+                        Title = "Playera de color Celeste",
+                        Description = "Fresca y ligera para el verano.",
+                        Price = 220,
+                        Image = "https://i.imgur.com/N1GkCIR.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 31,
+                        Title = "Pantalones cortos de color Caqui",
+                        Description = "Comodidad y frescura para días calurosos.",
+                        Price = 280,
+                        Image = "https://i.imgur.com/cBuLvBi.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 32,
+                        Title = "Gorra de color Blanco",
+                        Description = "Combina con cualquier atuendo casual.",
+                        Price = 130,
+                        Image = "https://i.imgur.com/KcT6BE0.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 33,
+                        Title = "Jersey de color Vino",
+                        Description = "Perfecto para climas frescos con estilo.",
+                        Price = 370,
+                        Image = "https://i.imgur.com/BZrIEmb.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 34,
+                        Title = "Cazadora de color Verde Oliva",
+                        Description = "Ideal para aventuras al aire libre.",
+                        Price = 600,
+                        Image = "https://i.imgur.com/wXuQ7bm.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 35,
+                        Title = "Blusa de color Fucsia",
+                        Description = "Dale un toque de color vibrante a tu look.",
+                        Price = 250,
+                        Image = "https://i.imgur.com/76HAxcA.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 36,
+                        Title = "Chaleco de color Gris Oscuro",
+                        Description = "Aporta calidez sin perder movilidad.",
+                        Price = 320,
+                        Image = "https://i.imgur.com/Wv2KTsf.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 37,
+                        Title = "Botines de color Caramelo",
+                        Description = "Estilo y comodidad en cada pisada.",
+                        Price = 700,
+                        Image = "https://i.imgur.com/R3iobJA.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 38,
+                        Title = "Camiseta de color Naranja",
+                        Description = "Alegra tu día con este color enérgico.",
+                        Price = 210,
+                        Image = "https://i.imgur.com/JQRGIc2.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 39,
+                        Title = "Sudadera de color Azul Marino",
+                        Description = "Perfecta para mantenerte abrigado y cómodo.",
+                        Price = 350,
+                        Image = "https://i.imgur.com/mp3rUty.jpeg",
+                    },
+                    new Product
+                    {
+                        Id = 40,
+                        Title = "Bufanda de color Azul Cobalto",
+                        Description = "El toque ideal para un look invernal.",
+                        Price = 140,
+                        Image = "https://i.imgur.com/N1GkCIR.jpeg",
                     }
+
 
                 );
         }
--- StoreApiTest/Migrations/20250221075529_AddMoreSeedDataProduct.Designer.cs
+++ StoreApiTest/Migrations/20250221075529_AddMoreSeedDataProduct.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StoreApiTest.Data;
 
@@ -11,9 +12,11 @@ using StoreApiTest.Data;
 namespace StoreApiTest.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20250221075529_AddMoreSeedDataProduct")]
+    partial class AddMoreSeedDataProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
