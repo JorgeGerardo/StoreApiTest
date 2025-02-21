f80533a731932acb4a62c5fda659fd041ec97cbd
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-20 20:15:58 -0700

    Cors configurated

--- StoreApiTest/Program.cs
+++ StoreApiTest/Program.cs
@@ -16,6 +16,18 @@ namespace StoreApiTest
             var SqlConnection = builder.Configuration.GetConnectionString("SqlServerCon");
             builder.Services.AddDbContext<StoreContext>(options => options.UseSqlServer(SqlConnection));
             DIService.InjectDependencies(builder);
+
+            //Cors addition:
+            builder.Services.AddCors(cors =>
+            {
+                cors.AddPolicy(name: "Cors_Angular",
+                    builder => builder.WithOrigins(@"http://localhost:4200")
+                        .AllowAnyHeader() // Permite cualquier encabezado
+                        .AllowAnyMethod() // Permite cualquier método HTTP
+                        .AllowCredentials() // Permite el envío de cookies o credenciales
+                );
+            });
+
             // Add services to the container.
 
             builder.Services.AddControllers();
@@ -24,6 +36,8 @@ namespace StoreApiTest
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+            app.UseCors("Cors_Angular");
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
